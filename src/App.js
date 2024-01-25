b8983062eeefa02412b0b418b23c9e762b4b46e1
erkamren <erkamren@gmail.com>
2024-01-25 21:16:51 +0300

    access

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import Certificate from "./pages/Certificate";
 import Plant from "./pages/Plant";
 import Process from "./pages/Process";
 import Arge from "./pages/Arge";
+import Consultancy from "./pages/Consultancy";
 import Product from "./pages/Product";
 import Encyclopedia from "./pages/Encyclopedia";
-import Consultancy from "./pages/Consultancy";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
@@ -17,17 +18,16 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/aboutus" element={<Aboutus />} />
-        <Route path="/certificate" element={<Certificate />} />
-        <Route path="/plant" element={<Plant />} />
-        <Route path="/01" element={<Product />} />
-        <Route path="/plant" element={<Plant />} />
-        <Route path="/process" element={<Process />} />
-        <Route path="/arge" element={<Arge />} />
-        <Route path="/consultancy" element={<Consultancy />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/encyclopedia" element={<Encyclopedia />} />
-        <Route path="/encyclopedia" element={<Encyclopedia />} />
+        <Route path="aboutus" element={<Aboutus />} />
+        <Route path="certificate" element={<Certificate />} />
+        <Route path="plant" element={<Plant />} />
+        <Route path="process" element={<Process />} />
+        <Route path="arge" element={<Arge />} />
+        <Route path="consultancy" element={<Consultancy />} />
+        <Route path="product" element={<Product />} />
+        <Route path="encyclopedia" element={<Encyclopedia />} />
+        <Route path="encyclopedia" element={<Encyclopedia />} />
+        <Route path="blog" element={<Blog />} />
       </Routes>
       <Footer />
     </Router>
